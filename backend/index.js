584ae8889da92b28400224d1166faaf8519af187
vibhu-sheladiya <vibhasheladiya3936@gmail.com>
2024-11-04 23:46:16 +0530

    inital

--- backend/index.js
+++ backend/index.js
@@ -29,11 +29,13 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, `./src/public`)));
-app.use("/v1", routes);
+
 
 app.get('/',(req,res)=>{
 res.json("hello")
 })
+// app.use("/v1", routes);
+
 app.use("/public/adminImg", express.static(path.join(__dirname, "./src/public/adminImg")));
 
 // Error handler middleware
